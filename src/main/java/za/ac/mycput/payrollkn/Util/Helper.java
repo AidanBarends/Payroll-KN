a1e01282d558a97f213128853c09ef96f76dc294
aidan-g-barends <aidanbarends95@gmail.com>
2026-05-12 13:43:48 +0200

    Updated helper to have a method for LocalDate

--- src/main/java/za/ac/mycput/payrollkn/Util/Helper.java
+++ src/main/java/za/ac/mycput/payrollkn/Util/Helper.java
@@ -2,6 +2,8 @@ package za.ac.mycput.payrollkn.Util;
 
 import org.apache.commons.validator.routines.EmailValidator;
 
+import java.time.LocalDate;
+
 public class Helper {
 
     public static boolean isNullOrEmpty(String string) {
@@ -14,5 +16,12 @@ public class Helper {
     public static boolean isValidEmail(String email){
         EmailValidator validator = EmailValidator.getInstance();
         return validator.isValid(email);
+    }
+
+    public static boolean isValidDate(LocalDate DOB){
+        if(DOB == null){
+            return false;
+        }
+        return true;
     }
 }
