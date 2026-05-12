b9f98f216dc24e3b57b6445340e4c04a16f9264c
aidan-g-barends <aidanbarends95@gmail.com>
2026-05-12 13:32:36 +0200

    Added EmailValidator Dependency Added Helper class in Util package

--- src/main/java/za/ac/mycput/payrollkn/Util/Helper.java
+++ src/main/java/za/ac/mycput/payrollkn/Util/Helper.java
@@ -1,4 +1,18 @@
 package za.ac.mycput.payrollkn.Util;
 
+import org.apache.commons.validator.routines.EmailValidator;
+
 public class Helper {
+
+    public static boolean isNullOrEmpty(String string) {
+        if (string == null || string.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isValidEmail(String email){
+        EmailValidator validator = EmailValidator.getInstance();
+        return validator.isValid(email);
+    }
 }
